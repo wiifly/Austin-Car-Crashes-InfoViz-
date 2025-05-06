5a5d02ef78c3b93394616c3175053a71059e6da0
Bryson <JBB9417@nyu.edu>
2025-05-06 18:45:55 +0800

    basic setup

---
+++ src/app/heatmap/page.jsx
@@ -0,0 +1,9 @@
+import PageLayout from '../../components/PageLayout';
+
+export default function MapPage() {
+  return (
+    <PageLayout title="Map View">
+      <p>This is the map view. Crash locations will be plotted here soon.</p>
+    </PageLayout>
+  );
+}
